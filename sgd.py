2b2306811a8f6d5becec3e1efee6dc4636476349
Naozumi Hiranuma <hiranumn@Naozumis-mac.local>
2016-04-19 05:58:31 -0700

    added base code

---
+++ sgd.py
@@ -0,0 +1,43 @@
+from math import exp
+import random
+import numpy as np
+
+# TODO: Calculate logistic
+def logistic(x):
+    pass
+
+# TODO: Calculate accuracy of predictions on data
+def accuracy(data, predictions):
+    pass
+
+class model:
+    def __init__(self, structure):
+        self.weights=[]
+        for i in range(len(structure)-1):
+            self.weights.append(np.random.normal(size=(structure[i], structure[i+1])))
+            
+    # TODO: Calculate prediction based on model
+    def predict(self, point):
+        pass
+
+    # TODO: Update model using learning rate and L2 regularization
+    def update(self, a, delta, eta, lam):
+        pass
+
+    # TODO: Perfrom the forward step of backpropagation
+    def feedforward(self, point):
+        pass
+    
+    # TODO: Backpropagate errors
+    def backpropagate(self, a, label):
+        pass
+
+    # TODO: Train your model
+    def train(self, data, epochs, rate, lam):
+        pass
+
+def submission(data, lam=0.00001):
+    m = model([data[0]["features"].shape[1], 1])
+    m.train(data, 100, 0.05, lam)
+    return m
+    
